5877dbd06c0fdd6c849e8d7836f7323d39ad9c27
Mike Olson <mwolson@member.fsf.org>
2026-01-07 19:56:49 -0500

    chore: bump version to 0.2.3

--- eglot-python-preset.el
+++ eglot-python-preset.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2025-2026 Michael Olson <mwolson@gnu.org>
 
-;; Version: 0.2.2
+;; Version: 0.2.3
 ;; Author: Michael Olson <mwolson@gnu.org>
 ;; Maintainer: Michael Olson <mwolson@gnu.org>
 ;; URL: https://github.com/mwolson/eglot-python-preset
